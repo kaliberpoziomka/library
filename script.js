cb2843828ec3534dbbd431c1b2581a5c003ed2e1
kaliberpoziomka <szubiczuk.krzysztof@gmail.com>
2020-05-28 21:15:14 +0200

    basic layout finished

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// global variables
+let card = document.querySelector(".card");
+let bookTitle = document.querySelector(".book-title");
+let bookAuthor = document.querySelector(".book-author");
+let bookPages = document.querySelector(".book-pages");
+let bookStatus = document.querySelector(".book-status");
+
+
+// main object
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+};
+
 
