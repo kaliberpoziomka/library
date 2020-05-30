8eda69c22c7b99a1ae914b84c149c1573c24f99f
kaliberpoziomka <szubiczuk.krzysztof@gmail.com>
2020-05-30 16:41:13 +0200

    finished layout and script, things to improve: colors, fonts, cards move to right when scroll appears

--- script.js
+++ script.js
@@ -4,6 +4,56 @@ let bookTitle = document.querySelector(".book-title");
 let bookAuthor = document.querySelector(".book-author");
 let bookPages = document.querySelector(".book-pages");
 let bookStatus = document.querySelector(".book-status");
+let footerButton = document.querySelector(".footer-button");
+let exitForm = document.querySelector(".exit-form");
+let cardInput = document.querySelector(".card-input");
+let formButton = document.querySelector(".form-button");
+
+
+// form variables
+let titleInput = document.getElementById('input-title');
+let authorInput = document.getElementById('input-author');
+let pagesInput = document.getElementById('input-pages');
+let statusInputYes = document.getElementById('input-status1');
+let statusInputNo = document.getElementById('input-status2');
+
+// form save data
+let savedTitle;
+let savedAuthor;
+let savedPages;
+let savedStatus;
+
+
+
+// button functions
+exitForm.addEventListener('click', () => {
+    cardInput.style.display = "none";
+});
+
+footerButton.addEventListener('click', () => {
+    cardInput.style.display = "block";
+});
+
+// form data sending from user
+document.querySelector('form').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    savedTitle = titleInput.value;
+    savedAuthor = authorInput.value;
+    savedPages = pagesInput.value;
+    savedStatus = statusInputYes.value;
+    
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    statusInputYes = '';
+    cardInput.style.display = "none";
+
+    let newCard = new Book(savedTitle, savedAuthor, savedPages, savedStatus);
+    newCard.createCard();
+});
 
 
 // main object
@@ -12,6 +62,24 @@ function Book(title, author, pages, status) {
     this.author = author;
     this.pages = pages;
     this.status = status;
+
+    
+
+    this.createCard = function() {
+        if (this.status == 'yes') {
+            this.status = 'Read';
+        } else {
+            this.status = 'Not read';
+        };
+
+        document.querySelector("main").innerHTML += 
+        `<div class="card">
+        <p class="book-title">${this.title}</p>
+        <p class="book-author">${this.author}</p>
+        <p class="book-pages">${this.pages}</p>
+        <p class="book-status">${this.status}</p>
+    </div>`;
+    };
 };
 
 
